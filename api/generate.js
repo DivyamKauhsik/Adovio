677930964e8af9d634c712d2447be2841b4358a9
DivyamKauhsik <kaushik.divyam@gmail.com>
2026-06-03 11:29:53 -0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
     const data = await response.json();
     return res.status(200).json(data);
 
-  } catch (err) {
+  } catch(err) {
     return res.status(500).json({ error: err.message });
   }
 }
